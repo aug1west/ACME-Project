b9241d300bc4ddc4332405289e4e57eecf60c6e5
August West <aug1west@gmail.com>
2021-07-25 12:35:04 -0400

    new UT with branch message

--- src/utils/Foo.java
+++ src/utils/Foo.java
@@ -1,9 +1,10 @@
 package utils;
 
 public class Foo {
-
+	
 	public static void main(String[] args) {
 		System.out.println("howdy");
 		System.out.println("hi from GitHub");
+		System.out.println("hi 'dave1' branch, from Eclipse");
 	}
 }
